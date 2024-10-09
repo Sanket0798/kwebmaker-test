30a9de87565731c4f5fd52ead0c24b0cebf9fc1b
Sanket0798 <rathodsanket2022@gmail.com>
2024-10-09 18:17:38 +0530

    Images & SVGs have been added.

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,7 @@
-// import localFont from "next/font/local";
-import { Inter } from "next/font/google";
 import { Poppins } from "next/font/google";
 import "./styles/globals.css";
 
 const poppins = Poppins({ weight: "400", subsets: ["latin"] });
-
-// const geistSans = localFont({
-//   src: "./styles/fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./styles/fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
 
 export const metadata = {
   title: "kwebmakers",
@@ -24,9 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      > */}
       <body className={`${poppins.className} antialiased`}>{children}</body>
     </html>
   );
