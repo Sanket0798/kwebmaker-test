d34ae6869f668c3c2f4732dde1e70b1f1b70fe20
Sanket0798 <rathodsanket2022@gmail.com>
2024-10-10 20:54:08 +0530

    About Section in HomePage has been modified and finalaized.

--- app/components/Home/About.jsx
+++ app/components/Home/About.jsx
@@ -1,41 +1,62 @@
 import React from "react";
 import Image from "next/image";
-import { RainbowSVG } from "@/public/assets/HomePage/About/svg/Rainbow";
+import { Lato } from "next/font/google";
+import dynamic from "next/dynamic";
+
+const Button = dynamic(() => import("../ui/Button"), {
+  ssr: false,
+  loading: () => <button>Loading...</button>,
+});
+
+const lato = Lato({
+  weight: ["400", "700"], // Use the required weights
+  subsets: ["latin"],
+});
 
 const About = () => {
   return (
     <>
       <section className="w-1206 mx-auto h-[602px] flex flex-row items-start justify-between">
-        <div className="w-[610px] flex flex-col h-[535px] justify-start">
+        <div className="w-[610px] flex flex-col h-[535px] justify-end">
           <p className="font-medium text-[18px]">About Astral Paints</p>
-          <h2 className="text-[30px] font-bold">
+          <h2 className="text-[30px] font-bold flex items-center gap-x-[10px] mt-[23px]">
             Bringing Your Dreams To Life
-            <span></span>
+            <span>
+              <Image
+                src="/assets/HomePage/About/AboutLine.jpg"
+                width={157}
+                height={9}
+                alt="Decorative line"
+                loading="lazy"
+              />
+            </span>
           </h2>
-          <p>
+          <p
+            className={`${lato.className} text-[16px] mt-[31px] text-[#656565] leading-[25px] w-[549px] h-[100px]`}
+          >
             Lorem ipsum dolor sit amet consectetur. Placerat elementum lobortis
             phasellus porttitor amet odio tempor. Ac molestie fames id urna dui
             posuere ultricies aliquam. Gravida et ac ac donec. Lacus est diam at
             in pharetra velit luctus id pellentesque.
           </p>
-          <p>
+          <p
+            className={`${lato.className} text-[16px] mt-[13px] text-[#656565] leading-[25px] w-[549px] h-[100px]`}
+          >
             Lorem ipsum dolor sit amet consectetur. Placerat elementum lobortis
             phasellus porttitor amet odio tempor. Ac molestie fames id urna dui
             posuere ultricies aliquam. Gravida et ac ac donec. Lacus est diam at
             in pharetra velit luctus id pellentesque.
           </p>
-          {/* <button className=" text-[#E70000] border-4 border-indigo-500/100 w-[109px] h-[38px] rounded-full font-semibold text-[12px] transition transform hover:-translate-y-1">
-          Enquire Now
-        </button> */}
-          <button className="bg-white hover:bg-gray-100 text-red-500 font-bold py-2 px-4 rounded">
-            Read More
-          </button>
+          <Button className="text-white bg-[#E70000] text-[12px] font-semibold mt-[25px] rounded-full">
+            Enquire Now
+          </Button>
         </div>
         <Image
           src="/assets/HomePage/About/AboutImg.jpg"
           width={583}
           height={535}
-          alt="About Img"
+          alt="Astral Paints representative image"
+          priority={true}
         />
       </section>
     </>
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,15 @@
-import { Poppins } from "next/font/google";
+import { Poppins, Lato } from "next/font/google";
 import "./styles/globals.css";
 
-const poppins = Poppins({ weight: "400", subsets: ["latin"] });
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+const lato = Lato({
+  weight: ["100", "300", "400", "700", "900"],
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "kwebmakers",
