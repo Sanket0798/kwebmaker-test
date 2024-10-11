7763f1640ec707e1b4e85bb58547a47ff00b845d
Sanket0798 <rathodsanket2022@gmail.com>
2024-10-11 12:38:48 +0530

    Blog Section has been added with dynamic data.

--- app/components/Home/Blog.jsx
+++ app/components/Home/Blog.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { blogPosts } from "@/app/utils/Data";
+import { blogPosts } from "@/app/utils/Data"; 
 import { Lato } from "next/font/google";
 import dynamic from "next/dynamic";
 
@@ -15,7 +15,9 @@ const lato = Lato({
   subsets: ["latin"],
 });
 
-const Blog = () => {
+const Blog = ({ blogs = [] }) => {
+  const displayBlogs = blogs.length > 0 ? blogs : blogPosts;
+
   return (
     <section className="relative w-full h-[712px] flex flex-row items-start justify-between">
       <div className="absolute left-0 top-0 h-full w-[15px] bg-gradient-to-b from-[#00AE44] to-[#00C1DE]"></div>
@@ -36,16 +38,16 @@ const Blog = () => {
         </h1>
 
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-[40px] h-[439px]">
-          {blogPosts.map((post, index) => (
+          {displayBlogs.map((blog, index) => (
             <div
-              key={post.id}
+              key={blog.slug || blog.id} 
               className={`relative overflow-hidden ${
                 index === 1 ? "col-span-2 row-span-2" : ""
               }`}
             >
               <Image
-                src={post.image}
-                alt={post.title}
+                src={blog.featuredImage?.node?.sourceUrl || blog.image}
+                alt={blog.title}
                 width={index === 1 ? 800 : 400}
                 height={index === 1 ? 600 : 300}
                 className="object-cover w-full h-full"
@@ -55,10 +57,10 @@ const Blog = () => {
                 <p
                   className={`${lato.className} text-white italic text-sm mb-[13px]`}
                 >
-                  {post.date}
+                  {blog.date ? new Date(blog.date).toLocaleDateString() : ''}
                 </p>
                 <h3 className="text-white text-[20px] font-semibold leading-[22px]">
-                  {post.title}
+                  {blog.title}
                 </h3>
                 {index === 1 && (
                   <Button className="text-[#00C1DE] font-semibold text-[12px] bg-white transition transform hover:-translate-y-1 mt-[20px]">
--- app/components/Home/DealerSection.jsx
+++ app/components/Home/DealerSection.jsx
@@ -1,32 +1,3 @@
-// "use client";
-// import React from "react";
-
-// const DealerSection = () => {
-//   return (
-//     <section className="relative w-full h-[358px]">
-//       <div className="absolute left-0 top-0 h-full w-[15px] bg-[#00AE44]"></div>
-
-//       <div className="bg-DealerBanner bg-no-repeat bg-cover bg-center h-[358px] flex flex-col items-center justify-start py-[84px] gap-y-[10px]">
-//         <h4 className="text-[18px] font-medium text-white">
-//           Join the Success Journey
-//         </h4>
-//         <h1 className="text-[30px] font-bold text-white">Become a Dealer</h1>
-//         <p className="text-[16px] leading-6 text-white w-[535px] h-[50px] text-center">
-//           Lorem ipsum dolor sit amet consectetur. Integer dui quam nisi in
-//           ornare. Nec in tristique et ultrices sit ullamcorper massa tempor et.
-//         </p>
-//         <button className="text-black font-semibold text-[12px] w-[109px] h-[38px] bg-white rounded-full transition transform hover:-translate-y-1">
-//           Read More
-//         </button>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default DealerSection;
-
-
-
 "use client";
 import React from "react";
 
